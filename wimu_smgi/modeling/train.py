3348e6dd133aadc27c7d5f8cbe4053a55dc33321
GrzeAron11 <01169653@pw.edu.pl>
2026-04-21 16:41:37 +0200

    add W&B logger

--- wimu_smgi/modeling/train.py
+++ wimu_smgi/modeling/train.py
@@ -1,10 +1,12 @@
 from lightning.pytorch.cli import LightningCLI
+from dotenv import load_dotenv
 
 from wimu_smgi.modeling.datamodule import MusicGenreDataModule
 from wimu_smgi.modeling.model import MusicGenreClassifier
 
 
 def main():
+    load_dotenv()
     LightningCLI(MusicGenreClassifier, MusicGenreDataModule)
 
 
